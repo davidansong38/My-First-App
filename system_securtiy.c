9ae3b5e7e7f1b424d1e795d9e6ac45161e7c5ecd
davidansong38 <40319589+davidansong38@users.noreply.github.com>
2019-05-31 05:13:19 +0000

    Update system_securtiy.c

--- system_securtiy.c
+++ system_securtiy.c
@@ -22,6 +22,11 @@ typedef struct Local_Security{
    char* usr_pswd;
    usrbackup_recovery_pswd usrb_recpswd;
    usr_recovery_email usr_rec_email;
+   union __System_Permissions{
+       signed write_file;
+       unsigned read_file;
+       unsigned short execute_file;
+   }sys_permission;
 }local_security, *LocalSecurity, **lsecurity;
 
 static struct Local_Security system_sec = {.usrname = "Admin", .usr_pswd = "XQBLNCEEK1992",12991102111221 , "dansong14@gmail.com.david"};
@@ -30,7 +35,7 @@ static inline void* lclsecurity(void*, ...);
 
 static inline int sys_security(usr_name username, const char* usr_rec_email, char nl, usrbackup_recovery_pswd usrbrec_pswd, ...){
    
-   printf("User name : %s\nEmail : %s%cPassword : %ld", username, usr_rec_email, nl, usrbrec_pswd);
+   printf("User name : %s\nEmail : %s%cPassword : %ld\n", username, usr_rec_email, nl, usrbrec_pswd);
 
    return 0;
 }
@@ -39,7 +44,12 @@ static inline int run_cpu(int, struct Local_Security*, ...);
 
 static inline void end_file();
 
+static inline const unsigned int sysm(int);
+
 int main(void){
+  system_sec.sys_permission.write_file = 0xac;
+  system_sec.sys_permission.read_file = 0010;
+  system_sec.sys_permission.execute_file = 0x6;
 
   static  LocalSecurity lsec;
   lsec = &system_sec;
@@ -50,6 +60,7 @@ int main(void){
   printf("%c", '\n');
   
   lclsecurity(lsec);
+  sysm((int)system_sec.sys_permission.write_file);
 
   return 00;
 }
@@ -118,4 +129,28 @@ int run_cpu(int sys_sec, struct Local_Security* lcalsec, ...){
 
   exit:
     return 0x0000u;
+}
+
+#define HINT 0xac
+
+const unsigned int sysm(int jump){
+
+  switch(jump){
+    case 0x0001:
+    case HINT:
+     puts("Access Granted to read a single file at a time...");
+     break;
+    case 0100:
+    case 0x90:
+    case 2332:
+     printf("Access Granted to read multiple file at different times...");
+     break;
+    case 0x666:
+    case 0x720:
+     printf("Access && Authorization granted to read and write file with execute permission....");
+     break;
+    default: 
+     puts("System offline for the moment....");
+  }
+  return 0;
 }
