896ade565b2eb96d44ecd1c4fc32aae19a80b0d7
davidansong38 <40319589+davidansong38@users.noreply.github.com>
2019-06-07 19:51:19 +0000

    Update oot.py

--- oot.py
+++ oot.py
@@ -1,3 +1,4 @@
+
 class SERVER__SOCKET:
   def __init__(self):
      self.__server_response = "Processing request....."
@@ -26,17 +27,53 @@ class CLIENT_SOC(SERVER__SOCKET):
            print("{}".format(self.__client_request))
     def write_req_to_file(self):
         return ("User Request :  "+ self.__client_success_message + "\n" + self.__client_syn_ack)
-           
+        
+def rec_req(recev_req = ""):
+        #recev_req.close()
+        return 255
+
+def run_req(wrte = ""):      
+       save_req = open("user_server_request.bin", "w")
+       save_req.write(wrte)
+       save_req.close()
+       return 0
 
 client_socket_connection = CLIENT_SOC()
 client_socket_connection.getters_client()
 
-save_req = open("user_server_request.bin", "w")
+rec_check = True
 
-if save_req.write(client_socket_connection.write_req_to_file()) == True:
-      save_req.close()
-elif save_req.write(client_socket_connection.write_req_to_file()) == 255:
-   print()
-   __request = input()
-   __request
+if rec_check == False:
+    run_req(client_socket_connection.write_req_to_file())
+elif rec_req() == 255:
+   print("Select request status during processing....")
+   print("@1.Request not sent.\n@2.Request sent successfully.\n@3.System not responding due to invalid input.")
+   __request = ""
+   __request = input("Enter your request response : ")
+   if __request == 1:
+     print("Please we're Kindly sorry for the interruption to the network connection. Please try re-connecting to the server. If this continue to be a problem to the connection, then please check or troubleshoot your network connection(such as router, switch, modem or network cables..Thank you. We're live because of you..#####")
+   elif __request == 0x0002:
+      print("Thank you for using our services. See you anytime..")
+   elif __request == 0x000003:
+      print("invalid input request. Please insert a valid input?")
+      if __request == 0x000003:
+          __request = input("Please Enter your status value again : ")
+          while __request == 0x000003:
+           print("invalid input request. Please insert a valid input?")
+           ++__request
+      else:
+          print("No request made or Please enter a status during the processing of your request for further analysis and performance")
+else:
+    print("System not responding....")
 
+south_africa = "South Africa"
+Africa_Eco = list(("Ghana", "Gambia", "Nigeria", south_africa))
+Africa_Eco.append("Libya")
+print("Current Country : " + Africa_Eco[0x0001])
+
+ghana_satellite_for_streets = ["Breya street", "Mukose street", "malam street", "Akoto street"]
+print("Your current Location on Ghana's GPS : "+ ghana_satellite_for_streets[2])
+
+current_tribes_in_ghana = dict(akans = 12, ewe = 9, muslims = 90)
+current_ghana_economic_situation = {"education" : 91.2282003, "total_fund" : 81.028981, "social_security" : "Not safe", "total_results_on_ghana_sys" : "Not great or Poor"} 
+print("Total output in Ghana's Economy : " + current_ghana_economic_situation["total_results_on_ghana_sys"])
